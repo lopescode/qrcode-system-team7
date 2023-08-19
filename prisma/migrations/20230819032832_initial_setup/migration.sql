50fd8f4609abfc6c197d8d8ac77d3c8b55489430
Tiago Matsushima <dev.matsu22@gmail.com>
2023-08-19 00:30:42 -0300

    setup do prisma e criação de tabelas

---
+++ prisma/migrations/20230819032832_initial_setup/migration.sql
@@ -0,0 +1,30 @@
+-- CreateTable
+CREATE TABLE `Costumer` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `table` INTEGER NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `Product` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `name` VARCHAR(191) NOT NULL,
+    `description` VARCHAR(191) NOT NULL,
+    `ingredients` VARCHAR(191) NOT NULL,
+    `photo` VARCHAR(191) NOT NULL,
+    `categoryId` INTEGER NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `Category` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `name` VARCHAR(191) NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- AddForeignKey
+ALTER TABLE `Product` ADD CONSTRAINT `Product_categoryId_fkey` FOREIGN KEY (`categoryId`) REFERENCES `Category`(`id`) ON DELETE SET NULL ON UPDATE CASCADE;
