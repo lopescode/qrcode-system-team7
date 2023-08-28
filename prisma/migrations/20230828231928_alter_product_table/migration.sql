d3245d352a4dffdd74c48b03292af78f7f795710
LOPES_PC\leona <leonardolopesb97@hotmail.com>
2023-08-28 20:19:48 -0300

    Alter product table migrate

---
+++ prisma/migrations/20230828231928_alter_product_table/migration.sql
@@ -0,0 +1,10 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `photo` on the `product` table. All the data in the column will be lost.
+  - Added the required column `imageUrl` to the `Product` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- AlterTable
+ALTER TABLE `product` DROP COLUMN `photo`,
+    ADD COLUMN `imageUrl` VARCHAR(191) NOT NULL;
